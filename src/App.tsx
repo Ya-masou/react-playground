9e3cda621a22c415c56c62f067c5252f664d9615
山宗一郎 <yamasouichirou@yamasouno-pro.local>
2021-03-24 09:43:39 +0900

    検証用の react セットアップ
    * 随時環境は整備していく予定

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>React を勉強するために検証用で作成したコンポーネントをここに並べていく</p>
+      <h2 className="section-title">テスト</h2>
+    </div>
+  );
+}
+
+export default App;
